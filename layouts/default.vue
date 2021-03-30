6d089b5f4831414ecc1c7ece83b36c2451f3e04a
Joao Pedro Rocha <joaopedro@wdhouse.com.br>
2021-03-30 02:17:42 -0300

    feat: add device detector to change the menu bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,20 @@
 <template>
 	<div>
-		<Nuxt />
+		<b-container>
+			<Header v-if="$device.isDesktop" />
+			<TabNavigation v-else/>
+			<Nuxt />
+		</b-container>
 	</div>
 </template>
+<script>
+import TabNavigation from '~/components/MobileApp/menu'
+import Header from '~/components/Desktop/Header'
+export default {
+	components:{
+		Header,
+		TabNavigation
+	},
+}
+</script>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,13 @@
 <template>
 	<div>
-		<TabNavigation/>
 		<courses/>
 	</div>
 </template>
 <script>
-import TabNavigation from '~/components/MobileApp/menu'
 import courses from '~/components/courses'
 export default {
 	components:{
 		courses,
-		TabNavigation
 	},
 }
 </script>
